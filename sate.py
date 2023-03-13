69a9a4ae2486a1597de3c85d002810193eea63c8
Лика <likakocura@gmail.com>
2023-03-13 21:05:04 +0300

    Начало БД

--- sate.py
+++ sate.py
@@ -1,4 +1,18 @@
+import sqlite3
+import os
 from flask import Flask, render_template, url_for, request
+
+
+DATABASE = './templates/flsite.db'
+DEBUG = True
+SECRET_KEY = 'jhgf789##234bjkvjhgfds,jht6'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
 servo1 = 1
 servoс = 100
 servo2 = 1
